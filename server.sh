79b3ed0b75217e13de8b2703949ad1c394e1dd43
NobleKangaroo <NobleKangaroo+Github@gmail.com>
2021-03-02 16:57:53 -0500

    Add '-batchmode' and '-nographics' arguments

--- server.sh
+++ server.sh
@@ -21,17 +21,20 @@ trap "kill -s SIGINT $!" SIGTERM
 
 # Start the server as a background job
 echo "Starting server:"
-echo " Name    : ${SERVER_NAME}"
-echo " Password: ${SERVER_PASSWORD}"
-echo " Port    : ${SERVER_PORT}"
-echo " World   : ${SERVER_WORLD}"
+echo "    Name    : ${SERVER_NAME}"
+echo "    Password: ${SERVER_PASSWORD}"
+echo "    Port    : ${SERVER_PORT}"
+echo "    World   : ${SERVER_WORLD}"
 /opt/valheim/server/valheim_server.x86_64 \
     -name "${SERVER_NAME}" \
     -password "${SERVER_PASSWORD}" \
     -port ${SERVER_PORT} \
     -world "${SERVER_WORLD}" \
     -savedir "/opt/valheim/data" \
-    -public 1 | tee -a /opt/valheim/server/server.log &
+    -public 1 \
+    -batchmode \
+    -nographics \
+    | tee -a "/opt/valheim/data/${SERVER_WORLD}.log" &
 
 # Wait for the job to end
 wait $!

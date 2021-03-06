c3d62344479798f7310bc6eebd1728f486be6cc5
NobleKangaroo <NobleKangaroo+Github@gmail.com>
2021-03-05 19:19:54 -0500

    Fix input redirection so '$!' is actually the server's PID

--- server.sh
+++ server.sh
@@ -31,7 +31,7 @@ echo "    World   : ${SERVER_WORLD}"
     -public 1 \
     -batchmode \
     -nographics \
-    | tee -a "/opt/valheim/data/${SERVER_WORLD}.log" &
+    > >(tee -a "/opt/valheim/data/${SERVER_WORLD}.log") &
 
 # Trap SIGTERM and perform safe shutdown
 trap "kill -s SIGINT $!" SIGTERM

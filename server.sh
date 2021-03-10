1bedbfef8d2c83a102715529f1ec8df9190072f1
NobleKangaroo <NobleKangaroo+Github@gmail.com>
2021-03-10 02:53:43 -0500

    Move server log file from data volume to server volume

--- server.sh
+++ server.sh
@@ -31,7 +31,7 @@ echo "    World   : ${SERVER_WORLD}"
     -public 1 \
     -batchmode \
     -nographics \
-    > >(tee -a "/opt/valheim/data/${SERVER_WORLD}.log") &
+    > >(tee -a "/opt/valheim/server/server.log") &
 
 # Trap SIGTERM and perform safe shutdown
 trap "kill -s SIGINT $!; wait" SIGTERM
